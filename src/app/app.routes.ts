b43d36831668034560c363899d268e83b2397610
Alexander <alexanderwu271206@gmail.com>
2025-02-10 10:45:04 +0100

    created header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
+import { QualificationComponent } from './pages/qualification/qualification.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'project/:id', component: ProjectDetailsComponent },
+  { path: 'qualification/', component: QualificationComponent },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
